c5a24c584366c9bfa82f7d32f7ec60d8b8c5ef89
Himanshu Maurya <121472539+i-himanshu29@users.noreply.github.com>
2024-09-07 04:22:00 +0000

    all container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,7 @@
 console.log("Himanshu")
+
+//Javascript environment - 'nodejs' to run javascript standalone.
+//Deno == nodejs both are the same working with the same founder
+
+// Install nodejs
+// use code editor - visual studio code
