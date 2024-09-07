8ab2c937e69868b90703c0c08627df7cc0dd5d78
Himanshu Maurya <121472539+i-himanshu29@users.noreply.github.com>
2024-09-07 05:11:54 +0000

    all about dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+4)// we can't use this syntax becoz we are useing nodejs not browser
+
+console.log(3+3); console.log("Hitesh")// wrost readablitiy
+// code reability should be high
+
+let name = "hitesh"
+let age =18
+let isloggedIn = false
+// number => 2 to power 53
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value hai
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof "hitesh");
+console.log(typeof hitesh);
+console.log(typeof age);
+console.log(typeof null); // object hai
+console.log(typeof undefined); // undefined
