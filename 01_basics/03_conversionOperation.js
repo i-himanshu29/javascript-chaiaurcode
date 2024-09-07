4d80784a2a848100ab70e9cfeb116c34dd1233d6
Himanshu Maurya <121472539+i-himanshu29@users.noreply.github.com>
2024-09-07 06:04:23 +0000

    all about conversion of DataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -58,3 +58,44 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+// ***********Operations*************
+console.log("Operations..................");
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+console.log("2+2 : ",2+2); //4
+console.log("2-2 : ",2-2) //0
+console.log("2*2 : ",2*2) //4
+console.log("2**3 : ",2**3);//8
+console.log("2/3 : ",2/3)//0.66666666
+console.log("2%3 : ",2%3);//2
+
+let str1="Hello"
+let str2 = "Himanshu"
+let str3 = str1 + str2
+console.log(str3)
+console.log(str1+" "+str2); //Hello Himanshu
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+"2"); //12
+console.log("1"+2+2);// 122
+console.log(1+2+"3") //33
+
+console.log((3+4) * 5 % 3);  //2
+
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+// let num1, num2, num3;
+// num1=num2=num3=2+2 
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log("gameCounter:",gameCounter); //101
+
+// Refer : https://tc39.es/ecma262/multipage/abstract-operations.html#sec-abstract-operations
