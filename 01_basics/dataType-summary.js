d9456bede3ed5baef50349edca1401e4208e62e0
Himanshu Maurya <121472539+i-himanshu29@users.noreply.github.com>
2024-09-07 06:56:49 +0000

    dataType-summary

---
+++ 01_basics/dataType-summary.js
@@ -0,0 +1,37 @@
+// JavaScript is a dynamic language and not static
+// Primitive -> call by value
+// 7 types: String , Number , Boolean , null , undefined , Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;//undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log("check",id===anotherId); //false
+
+// const bigNumber = 344556675435567944
+const bigNumber = 344556675435567944n
+
+//Non Primitive -> Reference Type
+// Array , Objects ,Functions 
+
+const heros = ["shaktiman","naagraj","doga"];
+//object
+let obj = {
+    name:"Himanshu",
+    age:22,
+}
+
+const myfunction = function(){
+    console.log("Jai Mata Di");
+}
+console.log("outsideTemp:",typeof(outsideTemp))
+console.log("scoreValue:",typeof scoreValue)
+console.log(typeof bigNumber);
+myfunction();
+console.log("function:",typeof myfunction)
+console.log("anotherId:",typeof anotherId)
