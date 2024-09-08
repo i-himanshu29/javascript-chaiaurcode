547f20329514e41bf63a4201ace3d1ef192a7291
Himanshu Maurya <121472539+i-himanshu29@users.noreply.github.com>
2024-09-08 13:35:17 +0000

    while and do while loop

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,41 @@
+// *************************************************
+
+// while and do-while 
+console.log("7.while and do-while ******************");
+
+let i=0;// intialisation
+while(i<=10){
+    console.log(`value of index is ${i}`);
+    i = i+2
+}
+console.log("8***************************");
+let myArray = ['flash','batman','superman']
+let arr =0;
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+}
+
+// do-while 
+// do{
+
+// }while(condition);
+console.log("9.do-while**************************");
+let score =1;
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10);
+
+console.log("10.do-while**************************");
+
+let score1 =11;
+do{
+    console.log(`score1 is ${score}`);
+    score1++
+}while(score1<=10);
+
+
+
+
+
